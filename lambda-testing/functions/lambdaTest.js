624fb0a61dca4e6854a7c787796e725b5008c3fc
CheolminConanShin <scm0226@gmail.com>
2017-06-05 10:37:07 +0900

    lambda testing

--- lambda-testing/functions/lambdaTest.js
+++ lambda-testing/functions/lambdaTest.js
@@ -1,6 +1,6 @@
 exports.handler = function(event, context) {
     if(event.key1) {
-      context.succeed(event.key1);
+      context.succeed("hello" + event.key1);
     } else {
       context.fail("no key1");
     }
